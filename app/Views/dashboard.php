2641714dd7bc78606c33345cb89cff2fa5d918e4
MARY <mary@MARYs-MacBook-Air.local>
2024-12-01 14:18:53 +0100

    2 éme commit

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tableau de bord</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/styleee.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
